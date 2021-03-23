f013b455d3ea4a177bbb2c59a0e1a22909c7ef97
Nicoll Oliver <nicolldon96@gmail.com>
2021-03-22 22:15:53 -0400

    forms to add

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-
+income_id = Income.create(yearly income: 19000).id
 #creating rental properties
 Rental.create(
     address: "130 West 2nd St Rear, Berwick, Pa 18603",
@@ -9,17 +9,31 @@ Rental.create(
     second_floor: false,
     pets_allowed: true,
     availability: "May 2021",
+    income_id: income_id,
     investor_id: Investor.find_or_create_by(name: "Nicholas Oliver").id
 )
 
 Rental.create(
+    address: "130 West 2nd St Apt. A, Berwick, Pa 18603",
+    bedrooms: "2 Bedrooms",
+    bathrooms: "1 Bath",
+    lease_length: "month-to-month",
+    monthly_rent: "500",
+    second_floor: true,
+    pets_allowed: false,
+    availability: "auto-renewed",
+    income_id: income_id,
+    investor_id: Investor.find_or_create_by(name: "Nicholas Oliver").id
+
+Rental.create(
     address: "130 West 2nd St. Berwick, Pa 18603",
-    bedrooms: "0 (STUDIO)",
-    bathrooms: "3/4 Bath",
+    bedrooms: "n/a - This is a commercial space",
+    bathrooms: "2 Commercial Baths",
     lease_length: "1 year",
     monthly_rent: "600",
     second_floor: false,
-    pets_allowed: true,
+    pets_allowed: false,
     availability: "February 2022",
+    income_id: income_id,
     investor_id: Investor.find_or_create_by(name: "Nicholas Oliver").id
 )
---
+++ notes.md
@@ -0,0 +1,6 @@
+## Questions for Laura
+
+rethink the relationship for income belongs to or has many through?
+
+data table for income: I want to make essentially a join table the connects both income and rentals to the investor.
+    I want the income controller to be able to grab all of the rental.monthly_rent and add it up from rentals.all to then multiply it by 12 to then render Yearly_income from all rentals - investors need to be able to see what they are projected to make.
